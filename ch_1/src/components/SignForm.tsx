61253efb748725a548af87990a68317ed31b63ac
최은석 <trgf456@naver.com>
2024-03-04 22:11:12 +0900

    [refac] ui 컴포넌트 분리

--- ch_1/src/components/SignForm.tsx
+++ ch_1/src/components/SignForm.tsx
@@ -3,24 +3,8 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { DevTool } from "@hookform/devtools";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
 
 import { useState } from "react";
 import NextArrow from "./icon/NextArrow";
