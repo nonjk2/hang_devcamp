482d071c9e7d2c14d2fb18379b657df27361d4d2
최은석 <trgf456@naver.com>
2024-03-07 21:17:38 +0900

    [init] 로그인후 폼 추가

--- ch_2/src/app/(auth)/home/page.tsx
+++ ch_2/src/app/(auth)/home/page.tsx
@@ -1,4 +1,21 @@
-const page = () => {
-  return <div>page</div>;
+import { authOption } from "@/app/api/auth/[...nextauth]/route";
+import AuthProfileForm from "@/components/AuthProfileForm";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { getServerSession } from "next-auth";
+
+const page = async () => {
+  const session = await getServerSession(authOption);
+
+  return (
+    <section>
+      {session ? (
+        <>
+          <AuthProfileForm session={session} />
+        </>
+      ) : (
+        <></>
+      )}
+    </section>
+  );
 };
 export default page;
---
+++ ch_2/src/components/AuthProfileForm.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { Session } from "next-auth";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { MouseEventHandler } from "react";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { RoleTypeName } from "@/lib/types/enum";
+
+const AuthProfileForm = ({ session }: { session: Session }) => {
+  const router = useRouter();
+  const onClickHandelr: MouseEventHandler<HTMLDivElement> = (e) => {
+    e.preventDefault();
+    signOut({ redirect: false }).then(() => router.push("/"));
+  };
+  return (
+    <div className="flex p-6 gap-2">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar className="cursor-pointer">
+            <AvatarImage src={session?.user?.image!} alt="@shadcn" />
+            <AvatarFallback>load</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>프로필 메뉴</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup>
+            <DropdownMenuRadioItem
+              value="right"
+              onClick={onClickHandelr}
+              className="cursor-pointer"
+            >
+              로그아웃
+            </DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <div className="flex flex-col items-center justify-center">
+        <span className="text-sm text-primary">{session?.user?.email}</span>
+        {session?.user?.role && (
+          <span className="text-sm text-primary">
+            {RoleTypeName[session.user.role]}
+          </span>
+        )}
+      </div>
+    </div>
+  );
+};
+export default AuthProfileForm;
