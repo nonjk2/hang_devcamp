ef5d46806218fff92f4858fb3d55e8bcb19ac312
최은석 <trgf456@naver.com>
2024-03-07 21:16:48 +0900

    [init] 로그인 유저 폼으로 진입불가 - middleware 추가

---
+++ ch_2/src/middleware.ts
@@ -0,0 +1,20 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export { default } from "next-auth/middleware";
+const secret = process.env.NEXTAUTH_SECRET;
+export async function middleware(req: NextRequest) {
+  const session = await getToken({ req, secret });
+  //   console.log("middleware :", session);
+
+  if (session) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/home";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
+export const config = {
+  matcher: ["/", "/signup"],
+};
