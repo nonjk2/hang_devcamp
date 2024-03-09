91a4f9eed33aec0960a1a88ffab7e7a6428bb6d4
최은석 <trgf456@naver.com>
2024-03-09 21:47:23 +0900

    [feat] 회원가입 로직 수정 - 이메일 중복 - 비밀번호 검사 - 오류 메세지 리턴

--- ch_2/src/lib/action/auth/action.ts
+++ ch_2/src/lib/action/auth/action.ts
@@ -1,43 +1,36 @@
 "use server";
 
-import { SignformSchema, supabaseAnon, supabaseURL } from "@/lib/constant";
-import { redirect } from "next/navigation";
-import { setTimeout } from "timers/promises";
+import { SignformSchema } from "@/lib/constant";
 import { z } from "zod";
-import { CombineFetch } from "..";
-import { createClient } from "@supabase/supabase-js";
-import { RoleTypeName, RoleTypeNameConvert } from "@/lib/types/enum";
+import { RoleTypeNameConvert } from "@/lib/types/enum";
+import { checkUserExists, createUser } from "./user";
 
 export const SignAction = async (formData: z.infer<typeof SignformSchema>) => {
-  const supabase = createClient(supabaseURL, supabaseAnon, {
-    db: { schema: "next-auth" },
-  });
-  const body = {
-    name: formData.이름,
-    email: formData.이메일,
-    phone: formData.연락처,
-    role: formData.역할,
-    password: formData.비밀번호,
-  };
+  const {
+    이름: name,
+    이메일: email,
+    연락처: phone,
+    역할: role,
+    비밀번호: password,
+  } = formData;
+  try {
+    const existingUser = await checkUserExists(email);
+    if (existingUser) {
+      return { message: "이메일이 중복 되었습니다." };
+    }
+    const newUser = await createUser({
+      email,
+      password,
+      name,
+      role: RoleTypeNameConvert[role] ?? "user",
+    });
+    console.log("newUser : ", newUser);
 
-  const { email, password, role, phone, name } = body;
-
-  const { error, data } = await supabase.auth.signUp({
-    email,
-    password,
-    phone,
-    options: {
-      data: {
-        name,
-        role: RoleTypeNameConvert[role] ?? "user",
-      },
-    },
-  });
-
-  if (error) {
-    throw new Error(error.message);
+    return { message: "회원가입성공" };
+  } catch (error) {
+    console.error("회원가입 오류 : ", error);
+    return { message: "회원가입오류" };
   }
-  console.log(data);
   // redirect("/");
   // const res = await CombineFetch<any>({
   //   path: "/api/users",
