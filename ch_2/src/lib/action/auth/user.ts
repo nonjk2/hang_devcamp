0160d1daba39b045ab71257b3db486853b1eb77a
최은석 <trgf456@naver.com>
2024-03-09 21:48:46 +0900

    [feat] 비동기 로직 파일 분리 - 유저확인 - 회원가입 -password 해싱 - 이메일 비밀번호 검증

---
+++ ch_2/src/lib/action/auth/user.ts
@@ -0,0 +1,88 @@
+import { createClient } from "@supabase/supabase-js";
+const bcrypt = require("bcryptjs");
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  { db: { schema: "next_auth" } }
+);
+
+interface UserFormData {
+  email: string;
+  password: string;
+  name: string;
+  role: string;
+}
+export async function checkUserExists(email: string) {
+  const { data: user, error } = await supabase
+    .from("users")
+    .select("email")
+    .eq("email", email)
+    .maybeSingle();
+
+  if (error && error.message !== "No rows found") {
+    throw error;
+  }
+
+  return user;
+}
+
+export async function createUser({
+  email,
+  password,
+  name,
+  role,
+}: UserFormData) {
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const { data, error } = await supabase.from("users").insert({
+    email,
+    password: hashedPassword,
+    name,
+    role,
+  });
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
+}
+export async function getUserByEmail(email: string) {
+  const { data: user, error } = await supabase
+    .from("users")
+    .select("*")
+    .eq("email", email)
+    .single();
+
+  if (error) {
+    console.error("Error fetching user:", error);
+    throw error;
+  }
+
+  return user;
+}
+
+export async function authenticateUser(email: string, password: string) {
+  try {
+    const user = await getUserByEmail(email);
+    if (!user) {
+      return null;
+    }
+
+    const isValid = bcrypt.compareSync(password, user.password);
+    if (!isValid) {
+      return null;
+    }
+
+    return {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      image: user.image,
+      role: user.role,
+    };
+  } catch (error) {
+    console.error("Authentication error : ", error);
+    return null;
+  }
+}
