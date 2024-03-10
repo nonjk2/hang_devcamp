f97f59031f7b6e35167cf0af4c565f6ee6f9fe82
최은석 <trgf456@naver.com>
2024-03-10 16:59:25 +0900

    [init] session 임시 non null 분기해제

--- ch_2/src/app/(auth)/home/page.tsx
+++ ch_2/src/app/(auth)/home/page.tsx
@@ -1,6 +1,5 @@
 import { authOption } from "@/app/api/auth/[...nextauth]/route";
 import AuthProfileForm from "@/components/AuthProfileForm";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { getServerSession } from "next-auth";
 
 const page = async () => {
@@ -8,13 +7,7 @@ const page = async () => {
 
   return (
     <section>
-      {session ? (
-        <>
-          <AuthProfileForm session={session} />
-        </>
-      ) : (
-        <></>
-      )}
+      <AuthProfileForm session={session!} />
     </section>
   );
 };
