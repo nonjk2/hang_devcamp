cc6192264b9011fb2558d5c760821ae6533c7c86
최은석 <trgf456@naver.com>
2024-03-13 22:16:18 +0900

    [init] 결제 페이지
    - 해당 페이지에서 RSC로 User와 유저가가진 쿠폰,포인트 비동기함수로 가져올예정 layout children 에 Suspense 추가 혹은 같은레벨에 loading컴포넌트 추가예정 ( 페이지 로드속도 사용자 경험)

--- ch_3/src/app/checkout/page.tsx
+++ ch_3/src/app/checkout/page.tsx
@@ -1,4 +1,45 @@
-const CheckOutPage = () => {
-  return <div>CheckOutPage</div>;
+import CheckOutAccountComponent from "@/components/CheckOutAccountComponent";
+import CheckOutCouponComponent from "@/components/CheckOutCouponComponent";
+import CheckOutFinalPayAgreeComponent from "@/components/CheckOutFinalPayAgreeComponent";
+import CheckOutItemsComponent from "@/components/CheckOutItemsComponent";
+import CheckOutPaymentComponent from "@/components/CheckOutPaymentComponent";
+import CheckOutUserAddressComponent from "@/components/CheckOutUserAddressComponent";
+import CheckOutUserInfoComponent from "@/components/CheckOutUserInfoComponent";
+import CheckoutProvider from "@/components/CheckoutProvider";
+import { mockCoupons, mockData } from "@/lib/mocks/mockdatas";
+
+const MOCK_USER: User = {
+  email: "hong@gildong.com",
+  id: "asdfsaf",
+  image: null,
+  nickname: "홍길동",
+  phone: "01099999999",
+  // address: null,
+};
+
+const MOCK_USER_HAS_POINTS = {
+  coupons: mockCoupons,
+  point: 2300,
+};
+
+const CheckOutPage = async () => {
+  return (
+    <CheckoutProvider productData={mockData}>
+      <div className="flex flex-col gap-4 grow">
+        <CheckOutItemsComponent />
+        <CheckOutUserInfoComponent user={MOCK_USER} />
+        <CheckOutUserAddressComponent user={MOCK_USER} />
+        <CheckOutCouponComponent {...MOCK_USER_HAS_POINTS} />
+      </div>
+      <div className="flex flex-col gap-4">
+        <CheckOutAccountComponent />
+        <CheckOutPaymentComponent />
+        <CheckOutFinalPayAgreeComponent />
+      </div>
+
+      {/* <CheckOutItemsComponent />
+      <CheckOutItemsComponent /> */}
+    </CheckoutProvider>
+  );
 };
 export default CheckOutPage;
