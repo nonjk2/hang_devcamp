e4c0640cba8ab35c436430869b387bce7ec64340
최은석 <trgf456@naver.com>
2024-03-13 22:17:37 +0900

    [init] 아이콘 추가

---
+++ ch_3/src/components/ui/icon/NextArrow.tsx
@@ -0,0 +1,20 @@
+const NextArrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="w-4 h-4 ml-2"
+    >
+      <path d="M5 12h14"></path>
+      <path d="m12 5 7 7-7 7"></path>
+    </svg>
+  );
+};
+export default NextArrow;
