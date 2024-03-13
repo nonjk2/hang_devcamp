1aefe039ae25bd60d2c0260035955e17bae01c9e
최은석 <trgf456@naver.com>
2024-03-13 22:18:12 +0900

    [util] 포인트 계산함수 , 한화 ' , ' 붙이는 함수 추가

--- ch_3/src/lib/utils.ts
+++ ch_3/src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(price: string) {
+  return new Intl.NumberFormat("ko-KR", {
+    currency: "KRW",
+    minimumFractionDigits: 0,
+  }).format(parseInt(price));
+}
+
+export function calculatePoints(totalPay: number): number {
+  const rate = 3.5 / 100;
+  let points = totalPay * rate;
+  points = Math.round(points / 100) * 100;
+  return points;
 }
