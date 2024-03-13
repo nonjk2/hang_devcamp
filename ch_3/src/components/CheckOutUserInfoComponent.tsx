5238c59906d79a66760e0b9fd3af39aa77a9d6c8
최은석 <trgf456@naver.com>
2024-03-13 22:13:09 +0900

    [feat] 주문자 정보 추가 - 변경사항없을시 해당 컴포넌트에서 랜더링 속도를 생각해 클라이언트 사이드로 바꿀예정

---
+++ ch_3/src/components/CheckOutUserInfoComponent.tsx
@@ -0,0 +1,21 @@
+import { formatPrice } from "@/lib/utils";
+import { Card, CardHeader, CardTitle, CardContent } from "./ui/card";
+
+const CheckOutUserInfoComponent = ({ user }: { user: User }) => {
+  return (
+    <Card className="rounded-sm">
+      <CardHeader>
+        <CardTitle className="text-lg">주문자 정보</CardTitle>
+      </CardHeader>
+      <CardContent className="flex gap-2 mt-3 justify-between">
+        <div className="flex flex-col">
+          <div className="">{user.nickname}</div>
+          {user.phone && <div className="">{user.phone}</div>}
+          <div className="">{user.email}</div>
+        </div>
+        <div></div>
+      </CardContent>
+    </Card>
+  );
+};
+export default CheckOutUserInfoComponent;
