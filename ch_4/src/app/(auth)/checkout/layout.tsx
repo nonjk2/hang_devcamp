c90e7d8ba1292eb184f640ea432957c53781ea96
최은석 <trgf456@naver.com>
2024-03-15 00:56:42 +0900

    [init] checkout layout 백그라운드 변화

--- ch_4/src/app/(auth)/checkout/layout.tsx
+++ ch_4/src/app/(auth)/checkout/layout.tsx
@@ -2,8 +2,10 @@ import { ReactNode } from "react";
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
-    <section className="flex max-w-screen-lg p-14 m-auto bg-slate-100 gap-4 max-sm:flex-col">
-      {children}
+    <section className="flex bg-background">
+      <div className="m-auto max-w-screen-lg p-14 gap-4 flex max-sm:flex-col">
+        {children}
+      </div>
     </section>
   );
 };
