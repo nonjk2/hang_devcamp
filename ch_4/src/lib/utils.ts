b8628c3997fd8d941d62e314155c63f21bebc7a7
최은석 <trgf456@naver.com>
2024-03-15 01:07:34 +0900

    [feat] 주소 데이터 추가 로직 (callback props)

--- ch_4/src/lib/utils.ts
+++ ch_4/src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { Address } from "react-daum-postcode";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -17,3 +18,24 @@ export function calculatePoints(totalPay: number): number {
   points = Math.round(points / 100) * 100;
   return points;
 }
+
+export const handleComplete = (
+  setAdress: (fullAdress: string, data: Address) => void,
+  data: Address
+) => {
+  let fullAddress = data.address;
+  let extraAddress = "";
+  console.log(data);
+  if (data.addressType === "R") {
+    if (data.bname !== "") {
+      extraAddress += data.bname;
+    }
+    if (data.buildingName !== "") {
+      extraAddress +=
+        extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+    }
+    fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+  }
+  console.log(fullAddress);
+  setAdress(fullAddress, data);
+};
