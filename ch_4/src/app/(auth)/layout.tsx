ba4cc95d0508da5407e4ad5fa073d9edf608bbbc
최은석 <trgf456@naver.com>
2024-03-15 00:53:55 +0900

    [feat] 어드민 분기처리 - 사용자 인증후 세션레이아웃

--- ch_4/src/app/(auth)/layout.tsx
+++ ch_4/src/app/(auth)/layout.tsx
@@ -1,6 +1,18 @@
+import { authOption } from "@/lib/action/auth/authoption";
+import { getServerSession } from "next-auth";
 import { ReactNode } from "react";
 
-const layout = ({ children }: { children: ReactNode }) => {
-  return <main>{children}</main>;
+const layout = async ({
+  children,
+  admin,
+}: {
+  children: ReactNode;
+  admin: ReactNode;
+}) => {
+  const session = await getServerSession(authOption);
+  if (!session?.user) {
+    <main>{children}</main>;
+  }
+  return <main>{session?.user.role === "admin" ? admin : children}</main>;
 };
 export default layout;
