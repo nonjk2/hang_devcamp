da85e4675aa20d38ffec454b8b18266c52157f8b
최은석 <trgf456@naver.com>
2024-03-19 19:55:22 +0900

    [feat] 관리자페이지 리다이렉트

--- ch_4/src/app/(auth)/@admin/home/page.tsx
+++ ch_4/src/app/(auth)/@admin/home/page.tsx
@@ -1,21 +1,12 @@
-import AuthProfileForm from "@/components/AuthProfileForm";
-import AdminCouponList from "@/components/admin/AdminCouponList";
-import AdminCouponSetting from "@/components/admin/AdminCouponSetting";
 import { authOption } from "@/lib/action/auth/authoption";
-import { getMyCoupons } from "@/lib/action/auth/user";
 import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 const page = async () => {
   const session = await getServerSession(authOption);
-  const useIssuedCoupon = await getMyCoupons(session?.user.id!);
-  return (
-    <div className="px-10 max-w-7xl m-auto">
-      <AuthProfileForm session={session!} />
-      <div className="flex w-full gap-3">
-        <AdminCouponSetting user={session?.user!} />
-        <AdminCouponList couponList={useIssuedCoupon} />
-      </div>
-    </div>
-  );
+  if (session?.user.role === "admin") {
+    redirect("/dashboard");
+  }
+  return null;
 };
 export default page;
